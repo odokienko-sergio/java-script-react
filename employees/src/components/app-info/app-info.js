cae95762ee7a7aed6b519c8adabcbd38c3258877
Serhii Odokiienko <58706778+odokienko-sergio@users.noreply.github.com>
2023-07-12 16:17:56 +0300

    Add files via upload

---
+++ employees/src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Employee accounting in company N</h1>
+            <h2>Total number of employees:</h2>
+            <h2>The following will receive the award:</h2>
+        </div>
+    )
+}
+
+export default AppInfo;
